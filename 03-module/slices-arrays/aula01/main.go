006f43877d6a03ceb4ed0ecf93c84a27749de68b
William Koller <williamkoller30@gmail.com>
2025-08-06 17:13:45 -0300

    chore: initial commit

---
+++ 03-module/slices-arrays/aula01/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	list := []int{4, 9, 6, 7} // listagem estatica
+	fmt.Println("List: ", list)
+	fmt.Println("List: ", list[0])
+	fmt.Println("List: ", list[1])
+	fmt.Println("List: ", len(list))
+}
---
+++ 03-module/slices-arrays/aula02/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	list := []int{4, 9, 6, 7}
+	fmt.Println("List: ", list)
+	fmt.Println("List: ", list[0])
+	fmt.Println("List: ", list[1])
+	fmt.Println("List lenght: ", len(list))
+
+	list = append(list, 8)
+
+	fmt.Println("List lenght: ", len(list))
+
+}
