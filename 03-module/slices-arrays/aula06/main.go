1be4c392aa01c7f2cb57427239f3feb12c902668
William Koller <williamkoller30@gmail.com>
2025-08-29 11:10:00 -0300

    chore: add aula 06

--- 03-module/slices-arrays/aula06/main.go
+++ 03-module/slices-arrays/aula06/main.go
@@ -8,7 +8,7 @@ func main() {
 	// var list = [10]int{1, 2, 3, 4, 5, 6, 7, 8} // is one array
 	var list = []int{1, 2, 3, 4, 5, 6, 7, 8} // is one slices
 
-	list = append(list, 0)
+	list = append(list, 1)
 
 	fmt.Printf("%T", list)
 }
